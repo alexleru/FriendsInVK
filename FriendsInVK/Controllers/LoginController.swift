9c023426479709398d94011ba621b4e3ec788cc1
Alexleru <alexeymoblive@gmail.com>
2019-08-21 10:42:34 +0300

    addNetworkSettingClass_and_WEBKIT_in_Controller

--- FriendsInVK/Controllers/LoginController.swift
+++ FriendsInVK/Controllers/LoginController.swift
@@ -7,46 +7,78 @@
 //
 
 import UIKit
+import WebKit
 
 class LoginController: UIViewController {
     //MARK: - outlets
-    @IBOutlet weak var usernameField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet var webView: WKWebView!{
+        didSet{
+            webView.navigationDelegate = self
+        }
+    }
+    let state = String(Int.random(in: 100000...999999))
     
     //MARK: - ControllerLifeCicle
     override func viewDidLoad() {
         super.viewDidLoad()
-        NetworkService().request()
-    }
-    //MARK: - Actions
-    @IBAction func loginButton(_ sender: Any) {
-
-    }
-    
-    //MARK: - Fuctions
-//    @objc func hideKeyboard() {
-//        scrollView.endEditing(true)
-//    }
-    
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        guard identifier == "Login Segue" else {return true}
         
-        if usernameField.text == "" && passwordField.text == ""
-        {
-            return true
-        }
-        else {
-            showLoginError()
-            return false
-        }
-    }
-    
-    private func showLoginError() {
-        let loginAlert = UIAlertController(title: "Error", message: "Wrong pair login and password", preferredStyle: .alert)
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "7099973"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.101"),
+            URLQueryItem(name: "state", value: state)
+        ]
         
-        let action = UIAlertAction(title: "Ok", style: .default) {_ in self.passwordField.text = ""}
-        loginAlert.addAction(action)
-        present(loginAlert, animated: true)
+        let request = URLRequest(url: urlComponents.url!)
+        
+        webView.load(request)
+    }
+}
+extension LoginController: WKNavigationDelegate {
+    internal func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url,
+            url.path == "/blank.html",
+            let fragment = url.fragment else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map{$0.components(separatedBy: "=")}
+            .reduce([String:String]()) {result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        print("++++++++++++++++++++++++++++++++")
+        print(params)
+        print("++++++++++++++++++++++++++++++++")
+        
+        guard let token = params["access_token"],
+            let userIdString = params["user_id"],
+            let userId = Int(userIdString) else {
+                decisionHandler(.allow)
+                return
+        }
+        
+        Session.instance.token = token
+        Session.instance.userId = userId
+        NetworkService().loadFriends()
+        NetworkService().loadGroups()
+        NetworkService().searchGrpups(for: "Ice Cream")
+        //NetworkService().loadPhotos()
+        
+        decisionHandler(.cancel)
     }
 }
--- FriendsInVK/NetworkService.swift
+++ FriendsInVK/NetworkService.swift
@@ -10,10 +10,63 @@ import Foundation
 import Alamofire
 
 class NetworkService{
+    //https://api.vk.com/method/friends.get?v=5.101&fields=city,nickname&access_token=
+    //MARK: - constant
+    let baseUrl = "https://api.vk.com"
+    let path = "/method"
+    let token = Session.instance.token
+    let version = 5.101
     
-    func password() {
-        AF.request("https://jsonplaceholder.typicode.com/posts/1/comments").responseJSON{response in
-            print(response.value!)
+    //MARK: - func
+    func loadFriends(){
+        let addpath = "/friends.get"
+        let parameters: Parameters = [
+            "access_token":token,
+            "v":version,
+            "fields":"nickname"
+        ]
+        AF.request(baseUrl+path+addpath, method: .get, parameters: parameters).responseJSON{response in
+            print("+++++++++++++FRIENDS+++++++++")
+            print(response)
         }
     }
+    func loadGroups(){
+        let addpath = "/groups.get"
+        let parameters: Parameters = [
+            "access_token":token,
+            "v":version,
+            "fields":"name"
+        ]
+        AF.request(baseUrl+path+addpath, method: .get, parameters: parameters).responseJSON{response in
+            print("+++++++++++++GROUPS+++++++++")
+            print(response)
+        }
+    }
+    func searchGrpups(for search: String){
+        let addpath = "/groups.search"
+        let parameters: Parameters = [
+            "access_token":token,
+            "v":version,
+            "q":search,
+            "type":"group"
+        ]
+        
+        AF.request(baseUrl+path+addpath, method: .get, parameters: parameters).responseJSON{response in
+            print("+++++++++++++GROUP_SEARCH+++++++++")
+            print(response)
+        }
+    }
+    func loadPhotos(){
+        let addpath = "/photos.get"
+        let parameters: Parameters = [
+            "access_token":token,
+            "v":version,
+            "owner_id":Session.instance.userId
+        ]
+        AF.request(baseUrl+path+addpath, method: .get, parameters: parameters).responseJSON{response in
+            print("+++++++++++++PHOTOS+++++++++")
+            print(response)
+        }
+    }
+    
 }
