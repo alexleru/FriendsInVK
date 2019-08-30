ebebbfbbeba565e82d6d2387fc170f155e8b393b
Alexleru <alexeymoblive@gmail.com>
2019-08-30 23:58:54 +0300

    first line

--- FriendsInVK/Model/Group.swift
+++ FriendsInVK/Model/Group.swift
@@ -2,7 +2,7 @@
 //  Group.swift
 //  FriendsInVK
 //
-//  Created by Darya Bodaykina on 13/08/2019.
+//  Created by alexleru on 13/08/2019.
 //  Copyright © 2019 alexleru. All rights reserved.
 //
 
@@ -15,14 +15,14 @@ class Group: Object {
     @objc dynamic var name: String = ""
     @objc dynamic var descriptions: String = ""
     
-    //let groupId: Int
-    //let name: String
-    //let description: String
-    
     required convenience init(_ json: JSON) {
         self.init()
         self.groupId = json["id"].intValue
         self.name = json["name"].stringValue
         self.descriptions = json["description"].stringValue
+    }
+    
+    override static func primaryKey() -> String? {
+        return "groupId"
     }
 }
