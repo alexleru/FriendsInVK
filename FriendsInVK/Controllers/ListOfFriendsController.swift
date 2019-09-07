ce17c8cb54e281824968c0259b71a257cdbb6628
Alexleru <alexeymoblive@gmail.com>
2019-09-08 01:23:21 +0300

    homework7

--- FriendsInVK/Controllers/ListOfFriendsController.swift
+++ FriendsInVK/Controllers/ListOfFriendsController.swift
@@ -10,42 +10,53 @@ import UIKit
 import Kingfisher
 import RealmSwift
 
+
 class ListOfFriendsController: UITableViewController {
 
     //MARK: - constants
+    private var friends: Results<Friend>? = nil
+    private var token: NotificationToken? = nil
     private let networkService = NetworkService();
-    private var friends = [Friend]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkService.loadFriends() {[weak self] in
-            self?.loadDataFriends()
-            self?.tableView.reloadData()
-        }
+        networkService.loadFriends()
+        loadDataFriends()
     }
     
     func loadDataFriends(){
-        do {
-            let realm = try Realm()
-            let friends = realm.objects(Friend.self)
-            self.friends = Array(friends)
-        } catch {
-            print(error)
-        }
+        let realm = try? Realm()
+        friends = realm?.objects(Friend.self)
+        token = (friends!.observe{ [weak self] (changes: RealmCollectionChange) in
+            guard let tableView = self?.tableView else {return}
+            switch changes {
+            case .initial:
+                tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modification):
+                tableView.beginUpdates()
+                tableView.deleteRows(at: deletions.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+                tableView.insertRows(at: insertions.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+                tableView.reloadRows(at: modification.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+                tableView.endUpdates()
+            case .error(let error):
+                print(error)
+            }
+            
+            })
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return friends.count
+        return friends!.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListOfFriendsCell", for: indexPath) as? ListOfFriendsCell else {fatalError("Problem with ListOfFriendsCell")}
-
-        cell.listOfFriendsLabel.text = ("\(friends[indexPath.row].firstName) \(friends[indexPath.row].lastName)")
-        let url = URL(string: friends[indexPath.row].photo)
+        let friend = friends![indexPath.row]
+        cell.listOfFriendsLabel.text = ("\(friend.firstName) \(friend.lastName)")
+        let url = URL(string: friend.photo)
         cell.listOfFriendsImage.kf.setImage(with: url)
         return cell
     }
@@ -54,7 +65,7 @@ class ListOfFriendsController: UITableViewController {
         if segue.identifier == "PhotoShowSegue",
         let destinationVC = segue.destination as? PhotosOfFriendController,
             let indexPath = tableView.indexPathForSelectedRow{
-            destinationVC.userId = friends[indexPath.row].userId
+            destinationVC.userId = friends![indexPath.row].userId
         }
     }
 }
--- FriendsInVK/Controllers/ListOfGroupsController.swift
+++ FriendsInVK/Controllers/ListOfGroupsController.swift
@@ -12,40 +12,49 @@ import RealmSwift
 class ListOfGroupsController: UITableViewController {
 
     //MARK - constants
-    private let networkService = NetworkService()
-    private var groups = [Group]()
+    private var groups: Results<Group>? = nil
+    private var token: NotificationToken? = nil
+    private let networkService = NetworkService();
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkService.loadGroups() {[weak self] in
-            self?.loadDataGroups()
-            self?.tableView.reloadData()
-        
-        }
-
+        networkService.loadGroups()
+        loadDataGroups()
     }
     
+    
     func loadDataGroups(){
-        do {
-            let realm = try Realm()
-            let groups = realm.objects(Group.self)
-            self.groups = Array(groups)
-        } catch {
-            print(error)
-        }
+        let realm = try? Realm()
+        groups = realm?.objects(Group.self)
+        token = (groups!.observe{ [weak self] (changes: RealmCollectionChange) in
+            guard let tableView = self?.tableView else {return}
+            switch changes {
+            case .initial:
+                tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modification):
+                tableView.beginUpdates()
+                tableView.deleteRows(at: deletions.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+                tableView.insertRows(at: insertions.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+                tableView.reloadRows(at: modification.map({IndexPath(row: $0, section: 0)}), with: .automatic)
+                tableView.endUpdates()
+            case .error(let error):
+                print(error)
+            }
+            
+            })
     }
     
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groups.count
+        return groups!.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListOfGroupsCell", for: indexPath) as? ListOfGroupsCell else { fatalError("Problem with ListOfGroupsCell")}
 
-        cell.listOfGroupsNameLabel.text = groups[indexPath.row].name
-        cell.listOfGroupsDescLabel.text = groups[indexPath.row].description
+        cell.listOfGroupsNameLabel.text = groups![indexPath.row].name
+        cell.listOfGroupsDescLabel.text = groups![indexPath.row].descriptions
 
         return cell
     }
